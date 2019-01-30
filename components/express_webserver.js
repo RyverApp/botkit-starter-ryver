5e6d34495dc190de0c835aabd9174e27dd4bf344
Luke Baulch <labaulcha@gmail.com>
2019-01-31 10:21:25 +1100

    Remove unused express middleware loading

--- components/express_webserver.js
+++ components/express_webserver.js
@@ -8,12 +8,6 @@ module.exports = function (controller) {
     webserver.use(bodyParser.json());
     webserver.use(bodyParser.urlencoded({ extended: true }));
 
-    // import express middlewares that are present in /components/express_middleware
-    var normalizedPath = require("path").join(__dirname, "express_middleware");
-    require("fs").readdirSync(normalizedPath).forEach(function (file) {
-        require("./express_middleware/" + file)(webserver, controller);
-    });
-
     webserver.listen(process.env.PORT || 3000, null, function () {
         debug('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
     });
